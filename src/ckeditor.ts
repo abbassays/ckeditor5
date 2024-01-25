9df1b45d208d4bd4ff6e84a5efdd91b2a8e20cdc
Ali Abbas <maliabbas2001@gmail.com>
2024-01-26 04:31:54 +0500

    initial commit

---
+++ src/ckeditor.ts
@@ -0,0 +1,179 @@
+/**
+ * @license Copyright (c) 2014-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from "@ckeditor/ckeditor5-editor-classic";
+
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import {
+  Bold,
+  Italic,
+  Subscript,
+  Superscript,
+  Underline,
+} from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import type { EditorConfig } from "@ckeditor/ckeditor5-core";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { FindAndReplace } from "@ckeditor/ckeditor5-find-and-replace";
+import {
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+} from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Highlight } from "@ckeditor/ckeditor5-highlight";
+import { GeneralHtmlSupport } from "@ckeditor/ckeditor5-html-support";
+import {
+  Image,
+  ImageCaption,
+  ImageInsert,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+} from "@ckeditor/ckeditor5-image";
+import { Indent, IndentBlock } from "@ckeditor/ckeditor5-indent";
+import { Link, LinkImage } from "@ckeditor/ckeditor5-link";
+import { List } from "@ckeditor/ckeditor5-list";
+import { MediaEmbed } from "@ckeditor/ckeditor5-media-embed";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { PasteFromOffice } from "@ckeditor/ckeditor5-paste-from-office";
+import { SelectAll } from "@ckeditor/ckeditor5-select-all";
+import { Style } from "@ckeditor/ckeditor5-style";
+import {
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+} from "@ckeditor/ckeditor5-table";
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
+import { Undo } from "@ckeditor/ckeditor5-undo";
+import { WordCount } from "@ckeditor/ckeditor5-word-count";
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+  public static override builtinPlugins = [
+    Alignment,
+    Autoformat,
+    BlockQuote,
+    Bold,
+    Essentials,
+    FindAndReplace,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    GeneralHtmlSupport,
+    Heading,
+    Highlight,
+    Image,
+    ImageCaption,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    SelectAll,
+    Style,
+    Subscript,
+    Superscript,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    Underline,
+    Undo,
+    WordCount,
+  ];
+
+  public static override defaultConfig: EditorConfig = {
+    toolbar: {
+      items: [
+        "style",
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "underline",
+        "subscript",
+        "superscript",
+        "blockQuote",
+        "|",
+        "fontBackgroundColor",
+        "fontSize",
+        "fontColor",
+        "fontFamily",
+        "highlight",
+        "-",
+        "bulletedList",
+        "numberedList",
+        "outdent",
+        "indent",
+        "alignment",
+        "|",
+        "link",
+        "imageUpload",
+        "imageInsert",
+        "mediaEmbed",
+        "insertTable",
+        "selectAll",
+        "|",
+        "undo",
+        "redo",
+        "findAndReplace",
+      ],
+      shouldNotGroupWhenFull: true,
+    },
+    language: "en",
+    image: {
+      toolbar: [
+        "imageTextAlternative",
+        "toggleImageCaption",
+        "imageStyle:inline",
+        "imageStyle:block",
+        "imageStyle:side",
+        "linkImage",
+      ],
+    },
+    table: {
+      contentToolbar: [
+        "tableColumn",
+        "tableRow",
+        "mergeTableCells",
+        "tableCellProperties",
+        "tableProperties",
+      ],
+    },
+	style: {
+		definitions: [
+			{
+				name: 'Hint',
+				element: 'p',
+				classes: [ 'hint' ]
+			}, 
+		]
+	}
+  };
+}
+
+export default Editor;
